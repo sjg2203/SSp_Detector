5aa8e07a4cb5420f304cf82b453a1969b6566ad3
S.J. Guillot <58807831+sjg2203@users.noreply.github.com>
2023-10-19 13:25:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 DISTNAME='SSp_Detector'
-VERSION='2023.10.19a0'
+VERSION='2023.10.19a1'
 DESCRIPTION='Sleep spindles detector'
 LONG_DESCRIPTION='SSp_Detector: open-source Python package to detect sleep spindles using absolute or relative power.'
 DESCRIPTION_CONTENT_TYPE='text/x-rst;charset=UTF-8'
