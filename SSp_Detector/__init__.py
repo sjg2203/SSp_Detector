c8238751c59482741127efab1093324f6ede09ba
S.J. Guillot <58807831+sjg2203@users.noreply.github.com>
2023-10-19 13:26:01 +0200

    Update __init__.py

--- SSp_Detector/__init__.py
+++ SSp_Detector/__init__.py
@@ -6,7 +6,7 @@ from outdated import warn_if_outdated
 logging.basicConfig(format="%(asctime)s|%(levelname)s|%(message)s",datefmt="%y-%b-%d %H:%M:%S")
 
 __author__="Simon J. Guillot <simon.guillot@inserm.fr>"
-__version__= "2023.10.19a0"
+__version__= "2023.10.19a1"
 
 #Warn if newer version is available
 warn_if_outdated("SSp_Detector",__version__)
