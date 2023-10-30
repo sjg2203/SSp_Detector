9c15d51fb184cfcbf54ff16d002b4fec34be4616
S.J. Guillot <58807831+sjg2203@users.noreply.github.com>
2023-10-30 14:51:22 +0100

    Update __init__.py

--- SSp_Detector/__init__.py
+++ SSp_Detector/__init__.py
@@ -1,12 +1,23 @@
 import logging
+import os
 from .ssp_detector import *
 from outdated import warn_if_outdated
+path=os.path.abspath(os.path.dirname(__name__))
 
 #Define logger
-logging.basicConfig(format="%(asctime)s|%(levelname)s|%(message)s",datefmt="%y-%b-%d %H:%M:%S")
+logging.basicConfig(format='%(asctime)s|%(levelname)s|%(message)',datefmt='%y-%b-%d %H:%M:%S')
 
-__author__="Simon J. Guillot <simon.guillot@inserm.fr>"
-__version__= "2023.10.28"
+__author__='Simon J. Guillot <simon.guillot@inserm.fr>'
+VERSION=''
+DISTNAME='SSp_Detector'
+about={}
+if not VERSION:
+    pr=DISTNAME.lower().replace("-","_").replace(" ","_")
+    with open(os.path.join(path,pr,'SSp_Detector\__version__.py')) as f:
+        exec(f.read(),about)
+else:
+    about['__version__']=VERSION
+__version__=about['__version__']
 
 #Warn if newer version is available
-warn_if_outdated("SSp_Detector",__version__)
+warn_if_outdated('SSp_Detector',__version__)
