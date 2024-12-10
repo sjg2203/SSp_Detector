95af3290f7b4906a9f4bfbbcfaf1b94f9a6e269c
S.J. Guillot <58807831+sjg2203@users.noreply.github.com>
2024-12-10 18:37:13 +0100

    Update setup.py
    Signed-off-by: S.J. Guillot <58807831+sjg2203@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except ImportError:
 path = os.path.abspath(os.path.dirname(__name__))
 
 DISTNAME = "SSp_Detector"
-VERSION = "2023.10.31.post1"
+VERSION = "2024.12.10"
 if not VERSION:
     pr = DISTNAME.lower().replace("-", "_").replace(" ", "_")
     with open(os.path.join(path, pr, pr, "__version__.py")) as v:
